eb0b0690e5edac683d90823997ae1fe5adbf5ce1
Isaias <sosaisaias250@gmail.com>
2021-08-30 18:15:35 -0300

    modifique los delay del archivo scroll.js

--- src/scroll.js
+++ src/scroll.js
@@ -1,25 +1,27 @@
-ScrollReveal().reveal('.services,skills,studies-courses,contact',{delay:850,duration: 1000,reset: true});
-ScrollReveal().reveal('.programation1',{delay:850,duration: 500,reset: true,easing: 'steps(13)'});
-ScrollReveal().reveal('.programation2',{delay:1100,duration: 800,reset: true,easing: 'steps(13)'});
+ var my_delay= 1000
+ScrollReveal().reveal('.services,skills,studies-courses,contact',{delay:my_delay,duration: 1000,reset: true});
+ScrollReveal().reveal('.programation1',{delay:my_delay,duration: 500,reset: true,easing: 'steps(13)'});
+ScrollReveal().reveal('.programation2',{delay:my_delay,duration: 800,reset: true,easing: 'steps(13)'});
 ScrollReveal().reveal('.programation3,that-makes,what-i-learned',{delay:1300,duration: 1000,reset: true,easing: 'steps(13)'});
 ScrollReveal().reveal('.title',{delay:1000,duration: 1000,reset: true});
-ScrollReveal().reveal('.that-makes',{delay:1500,duration: 1500,reset: true});
-ScrollReveal().reveal('.what-i-learned',{delay:2000,duration: 1900,reset: true});
+ScrollReveal().reveal('.that-makes',{delay:my_delay,duration: 1500,reset: true});
+ScrollReveal().reveal('.what-i-learned',{delay:my_delay,duration: 1900,reset: true});
 
-ScrollReveal().reveal('.project1',{delay:1000,duration: 900,reset: true});
-ScrollReveal().reveal('.scripts',{delay:1300,duration: 1200,reset: true});
 
-ScrollReveal().reveal('.my-skills',{delay:900,duration: 900,reset: true});
-ScrollReveal().reveal('.pPy',{delay:400,duration: 700,reset: true});
-ScrollReveal().reveal('.pDj',{delay:800,duration: 700,reset: true});
-ScrollReveal().reveal('.pSql',{delay:1200,duration: 200,reset: true});
-ScrollReveal().reveal('.pGit',{delay:1600,duration: 700,reset: true});
-ScrollReveal().reveal('.practices',{delay:2000,duration: 700,reset: true});
+ScrollReveal().reveal('.project1',{delay:my_delay,duration: 900,reset: true});
+ScrollReveal().reveal('.scripts',{delay:my_delay,duration: 1200,reset: true});
 
-ScrollReveal().reveal('.studies-terminated',{delay:1000,duration: 2000,reset: true});
-ScrollReveal().reveal('.studies-in-process',{delay:1400,duration: 2500,reset: true});
-ScrollReveal().reveal('#py',{delay:1700,duration: 1100,reset: true});
-ScrollReveal().reveal('#sql',{delay:1900,duration: 1100,reset: true});
+ScrollReveal().reveal('.my-skills',{delay:my_delay,duration: 900,reset: true});
+ScrollReveal().reveal('.pPy',{delay:my_delay,duration: 700,reset: true});
+ScrollReveal().reveal('.pDj',{delay:my_delay,duration: 700,reset: true});
+ScrollReveal().reveal('.pSql',{delay:my_delay,duration: 200,reset: true});
+ScrollReveal().reveal('.pGit',{delay:my_delay,duration: 700,reset: true});
+ScrollReveal().reveal('.practices',{delay:my_delay,duration: 700,reset: true});
 
-ScrollReveal().reveal('#formContact',{delay:1200,duration: 1500,reset: true});
+ScrollReveal().reveal('.studies-terminated',{delay:my_delay,duration: 2000,reset: true});
+ScrollReveal().reveal('.studies-in-process',{delay:my_delay,duration: 2500,reset: true});
+ScrollReveal().reveal('#py',{delay:my_delay,duration: 1100,reset: true});
+ScrollReveal().reveal('#sql',{delay:my_delay,duration: 1100,reset: true});
+
+ScrollReveal().reveal('#formContact',{delay:my_delay,duration: 500,reset: true});
 ScrollReveal().reveal('.upload',{reset: true});
